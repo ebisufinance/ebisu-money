5fedbeeba676688d941e7e7d56d529df224255f6
amitay2 <amitay@dcentralab.com>
2024-10-05 15:47:34 +0300

    remove consoles

--- contracts/src/PriceFeeds/WEETHPriceFeed.sol
+++ contracts/src/PriceFeeds/WEETHPriceFeed.sol
@@ -5,8 +5,8 @@ pragma solidity 0.8.18;
 import "./CompositePriceFeed.sol";
 import "../Interfaces/IWEETHToken.sol";
 import "../Interfaces/IWEETHPriceFeed.sol";
-import "forge-std/console.sol";
-// import "forge-std/console2.sol";
+// import "forge-std/console.sol";
+
 
 contract WEETHPriceFeed is CompositePriceFeed, IWEETHPriceFeed {
 
@@ -24,9 +24,9 @@ contract WEETHPriceFeed is CompositePriceFeed, IWEETHPriceFeed {
         weEthEthOracle.aggregator = AggregatorV3Interface(_weEthEthOracleAddress);
         weEthEthOracle.stalenessThreshold = _weEthEthStalenessThreshold;
         weEthEthOracle.decimals = weEthEthOracle.aggregator.decimals();
-        console.log("before");
+        
         _fetchPricePrimary();
-        console.log("after");
+        
         // Check the oracle didn't already fail
         assert(priceSource == PriceSource.primary);
     }
@@ -34,24 +34,17 @@ contract WEETHPriceFeed is CompositePriceFeed, IWEETHPriceFeed {
     function _fetchPricePrimary() internal override returns (uint256, bool) {
         assert(priceSource == PriceSource.primary);
         (uint256 ethUsdPrice, bool ethUsdOracleDown) = _getOracleAnswer(ethUsdOracle);
-        console.log("weEthEthOracle decimals: ", weEthEthOracle.stalenessThreshold);
         (uint256 weEthEthPrice, bool weEthEthOracleDown) = _getOracleAnswer(weEthEthOracle);
-        console.log("before2");
         (uint256 weEthPerEth, bool exchangeRateIsDown) = _getCanonicalRate();
-        console.log("before3");
         // If the ETH-USD feed is down, shut down and switch to the last good price seen by the system
         // since we need both ETH-USD and canonical for primary and fallback price calcs
         if (ethUsdOracleDown || exchangeRateIsDown) {
-            console.log("ethUsdOracleDown: ",ethUsdOracleDown);
-            console.log("exchangeRateIsDown: ",exchangeRateIsDown);
             return (_shutDownAndSwitchToLastGoodPrice(address(ethUsdOracle.aggregator)), true);
         }
-        console.log("before4s");
         // If the ETH-USD feed is live but the WEETH-ETH oracle is down, shutdown and substitute WEETH-ETH with the canonical rate
         if (weEthEthOracleDown) {
             return (_shutDownAndSwitchToETHUSDxCanonical(address(weEthEthOracle.aggregator), ethUsdPrice), true);
         }
-        console.log("before5");
         // Otherwise, use the primary price calculation:
 
         // Calculate the market LRT-USD price: USD_per_WEETH = USD_per_ETH * ETH_per_WEETH
@@ -64,9 +57,7 @@ contract WEETHPriceFeed is CompositePriceFeed, IWEETHPriceFeed {
         // Take the minimum of (market, canonical) in order to mitigate against upward market price manipulation.
         // NOTE: only needed
         uint256 lrtUsdPrice = LiquityMath._min(lrtUsdMarketPrice, lrtUsdCanonicalPrice);
-        console.log("before5");
         lastGoodPrice = lrtUsdPrice;
-        console.log("lastGoodPrice: ",lastGoodPrice);
         return (lrtUsdPrice, false);
     }
 
